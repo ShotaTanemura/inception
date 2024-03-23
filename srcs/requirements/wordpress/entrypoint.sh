c7f9decab3c214cfb5de3f86fd1e9a742c5b2d6b
ShotaTanemura <shota0711tane@gmail.com>
2024-03-23 22:47:10 +0900

    install wordpress in entrypoint.sh

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -8,4 +8,7 @@ sed -i "s/\MYSQL_DATABASE/${MYSQL_DATABASE}/g" "$wp_config_file"
 sed -i "s/\MYSQL_USER/${MYSQL_USER}/g" "$wp_config_file"
 sed -i "s/\MYSQL_PASSWORD/${MYSQL_PASSWORD}/g" "$wp_config_file"
 
+cd /var/www/html/wordpress 
+wp core install --url=localhost --title=Example --admin_user=${MYSQL_USER} --admin_password=${MYSQL_PASSWORD} --admin_email=info@example.com --allow-root
+
 php-fpm8.3 -F
