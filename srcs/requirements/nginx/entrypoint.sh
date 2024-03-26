3db5feac76636708a611ecb02a68d1b4aa7501dc
ShotaTanemura <shota0711tane@gmail.com>
2024-03-26 21:26:58 +0900

    Use self certification instead of public certification

--- srcs/requirements/nginx/entrypoint.sh
+++ srcs/requirements/nginx/entrypoint.sh
@@ -6,7 +6,4 @@ config_file="/etc/nginx/conf.d/default.conf"
 
 sed -i "s/DOMAIN_NAME/${DOMAIN_NAME}/g" "$config_file"
 
-certbot --nginx -d ${DOMAIN_NAME}
-certbot renew --dry-run
-
 nginx -g 'daemon off;'
