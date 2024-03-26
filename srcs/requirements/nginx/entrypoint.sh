c40042f199d77121a97b0b74e0eb76bb37ce12ee
ShotaTanemura <shota0711tane@gmail.com>
2024-03-26 20:02:02 +0900

    Install certbot and setup https server with it

--- srcs/requirements/nginx/entrypoint.sh
+++ srcs/requirements/nginx/entrypoint.sh
@@ -5,4 +5,8 @@ set -ex;
 config_file="/etc/nginx/conf.d/default.conf"
 
 sed -i "s/DOMAIN_NAME/${DOMAIN_NAME}/g" "$config_file"
+
+certbot --nginx -d ${DOMAIN_NAME}
+certbot renew --dry-run
+
 nginx -g 'daemon off;'
