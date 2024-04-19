f2b00e7774009028b178444d7641e4ee9c1d2b62
ShotaTanemura <shota0711tane@gmaail.com>
2024-04-19 15:07:05 +0900

    Settin up a user for a client on redis

--- srcs/requirements/redis/entrypoint.sh
+++ srcs/requirements/redis/entrypoint.sh
@@ -1,3 +1,18 @@
 #! /bin/bash
 
+set -ex
+
+# protected-mode no
+sed -i 's/^\(protected-mode yes\)/# \1/g' /etc/redis/redis.conf
+
+# requirepass
+sed -i "s/^#\s*\(requirepass\) .*/\1 ${REDIS_ADMIN_PASSWORD}/g" /etc/redis/redis.conf
+
+redis-server --daemonize yes
+
+echo "AUTH ${REDIS_ADMIN_PASSWORD}" | redis-cli -x && \
+echo "ACL SETUSER ${REDIS_USER} on >${REDIS_PASSWORD} +@all" | redis-cli -x
+
+redis-cli shutdown
+
 redis-server
