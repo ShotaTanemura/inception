e7404956a431fc3328177ee26a6e4ed1f5a1ec2b
ShotaTanemura <shota0711tane@gmaail.com>
2024-04-20 13:44:38 +0900

    Remove unused redis environmental variables and rename one of them

--- srcs/requirements/redis/entrypoint.sh
+++ srcs/requirements/redis/entrypoint.sh
@@ -7,6 +7,6 @@ sed -i 's/^\(protected-mode yes\)/# \1/g' /etc/redis/redis.conf
 sed -i '/bind/ s/^/# /' /etc/redis/redis.conf
 
 # requirepass
-sed -i "s/^#\s*\(requirepass\) .*/\1 ${REDIS_ADMIN_PASSWORD}/g" /etc/redis/redis.conf
+sed -i "s/^#\s*\(requirepass\) .*/\1 ${REDIS_PASSWORD}/g" /etc/redis/redis.conf
 
 redis-server /etc/redis/redis.conf --daemonize no
