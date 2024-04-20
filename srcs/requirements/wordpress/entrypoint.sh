c0ab5944ba27295c6dfba81241ed11eda13cd6b0
ShotaTanemura <shota0711tane@gmail.com>
2024-04-20 13:32:47 +0900

    replace a line including an empty argument for constant value with environmental variable

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -8,13 +8,17 @@ sed -i "s/\MYSQL_DATABASE/${MYSQL_DATABASE}/g" "$wp_config_file"
 sed -i "s/\MYSQL_USER/${MYSQL_USER}/g" "$wp_config_file"
 sed -i "s/\MYSQL_PASSWORD/${MYSQL_PASSWORD}/g" "$wp_config_file"
 
-sed -i "s/\REDIS_USER/${REDIS_USER}/g" "$wp_config_file"
-sed -i "s/\REDIS_PASSWORD/${REDIS_PASSWORD}/g" "$wp_config_file"
+sed -i "s/define('WP_REDIS_PASSWORD', );/define('WP_REDIS_PASSWORD', ['default', '${REDIS_ADMIN_PASSWORD}']);/g" "$wp_config_file"
 
 cd /var/www/html/wordpress 
 wp core install --url=${DOMAIN_NAME} --title=${WORDPRESS_TITLE} --admin_user=${MYSQL_USER} --admin_password=${MYSQL_PASSWORD} --admin_email=${WORDPRESS_EMAIL} --allow-root
 wp search-replace "http://${DOMAIN_NAME}" "https://${DOMAIN_NAME}" --allow-root
 
-wp plugin install redis-cache --allow-root --path=/var/www/html/wordpress --activates;
+wp plugin install redis-cache --allow-root --path=/var/www/html/wordpress --activate
+
+# wp config set WP_REDIS_PASSWORD "[default, ${REDIS_ADMIN_PASSWORD}]" --allow-root
+
+
+# wp redis enable --allow-root
 
 php-fpm8.3 -F
